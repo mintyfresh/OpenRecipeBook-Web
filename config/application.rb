b395306f26ee59a4d36ff390165fb83fe952108f
Minty Fresh <mintiewute@gmail.com>
2020-02-11 04:43:12 -0500

    Add root application path.

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 
+require 'pathname'
+
 class Application < Sinatra::Base
   private_class_method :new
-
-  set :views, File.expand_path('../views', __dir__)
 
   # @return [Zeitwerk::Loader]
   def self.loader
     @loader ||= Zeitwerk::Loader.new
   end
 
+  # @return [Pathname]
+  def self.root
+    @root ||= Pathname.new(File.expand_path('..', __dir__)).freeze
+  end
+
+  set :views, root.join('views')
+
   loader.enable_reloading
-  loader.push_dir(File.expand_path('../lib', __dir__))
+  loader.push_dir(root.join('lib'))
   loader.setup
 end
